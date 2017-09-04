d0698ad1e5be14c3d77973e3f8273a015ef406f5
Alesia <jusfanio@gmail.com>
2017-09-04 22:18:01 +0300

    Initial Commit

---
+++ g56l3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  g56l3
+//
+//  Created by Alesia on 04/09/2017.
+//  Copyright © 2017 Alesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
